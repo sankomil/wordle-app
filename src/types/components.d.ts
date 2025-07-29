8eed79805f40376e09ed4577b921827fcf2a354e
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-29 20:23:55 +0800

    type: receive hook vars as props

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -2,3 +2,10 @@ export interface ILetter {
   value: string;
   status: "correct" | "misplaced" | "incorrect" | "invalidated";
 }
+
+export interface IGameBoard {
+  attempts: number;
+  turn: number;
+  guesses: ILetter[][];
+  currentGuess: string;
+}
