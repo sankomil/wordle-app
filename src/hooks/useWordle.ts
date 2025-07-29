899e84634f8be61d67ab10c59ab3fa1ddcff947a
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-29 15:11:52 +0800

    feat: add initial validation for guesses to hook

--- src/hooks/useWordle.ts
+++ src/hooks/useWordle.ts
@@ -7,26 +7,68 @@ export const useWordle = () => {
 
   const selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-  const [gameBoard, setGameBoard] = useState<ILetter[][]>(
-    new Array(attempts).fill(
-      new Array(5).fill({
-        value: "",
-        status: "invalidated",
-      })
-    )
-  );
+  const [guesses, setGuesses] = useState<ILetter[][]>([]);
 
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
 
-  const handleKeyPress = () => {};
+  const isGameOver = () => {
+    return turn + 1 === attempts || currentGuess === selectedWord;
+  };
+  const validateCurrGuess = () => {
+    let solArray = selectedWord.split("");
+    let currGuessArray = currentGuess
+      .split("")
+      .map((l) => ({ value: l, status: "invalidated" } as ILetter));
+    currGuessArray.forEach((l, i) => {
+      if (solArray[i] === l.value) {
+        currGuessArray[i].status = "correct";
+        solArray[i] = null;
+      }
+    });
+
+    currGuessArray.forEach((l, i) => {
+      if (solArray.includes(l.value) && l.status !== "correct") {
+        currGuessArray[i].status = "misplaced";
+        solArray[solArray.indexOf(l.value)] = null;
+      }
+    });
+
+    return currGuessArray;
+  };
+
+  const addCurrGuess = () => {
+    if (isGameOver()) {
+      return;
+    }
+    setGuesses((prev) => [...prev, validateCurrGuess()]);
+    setTurn((prev) => prev + 1);
+    setCurrentGuess("");
+  };
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      if (currentGuess.length === 5 || currentGuess.length < 5) {
+        return;
+      } else {
+        addCurrGuess();
+      }
+    } else if (e.key === "Backspace") {
+      setCurrentGuess((prevGuess) => prevGuess.slice(0, -1));
+      return;
+    } else if (/^[A-Za-z]$/.test(e.key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => prev + e.key.toUpperCase());
+      }
+    }
+  };
 
   const handleButtonPress = () => {};
   return {
     turn,
     currentGuess,
-    gameBoard,
+    guesses,
     handleKeyPress,
     handleButtonPress,
     attempts,
