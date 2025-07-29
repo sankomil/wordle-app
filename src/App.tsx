716e48f8e1d62deae295a2bf324eeede03cab69c
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-29 10:52:28 +0800

    style: remove template page.load from env

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const attempts = parseInt(process.env.REACT_APP_ATTEMPTS || "5");
+  const wordList = JSON.parse(process.env.REACT_APP_WORDLE_LIST || "[]");
+
+  const selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle</h1>
+      <p>Number of attempts: {attempts}</p>
     </div>
   );
 }
