46c5dd90ccbd43be29a177b7101255efdf478a95
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-30 18:10:28 +0800

    feat: port over validation logic

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 app = Flask(__name__)
 
 WORD_POOL = os.getenv("WORD_POOL").split(',')
-SELECTED_WORD = random.choice(WORD_POOL)
+SELECTED_WORD = random.choice(WORD_POOL).upper()
 ATTEMPTS = int(os.getenv("ATTEMPTS"))
 
 turns = 0
@@ -18,7 +18,7 @@ print(SELECTED_WORD, ATTEMPTS)
 @app.route("/validate", methods=["POST"])
 def validate_word():
     data = request.get_json()
-    guess = data.get("guess")
+    guess = data.get("guess").upper()
 
     if not guess:
         return jsonify({"error": "Guess not provided"}), 400
@@ -26,10 +26,35 @@ def validate_word():
     if len(guess) != 5:
         return jsonify({"error": "The guess should be five letters long"}), 400
 
-    if guess == SELECTED_WORD:
-        return jsonify({"correct": SELECTED_WORD})
-    else:
-        return jsonify({"correct": SELECTED_WORD})
+    sol_array = list(SELECTED_WORD)
+    validated_guess = [{"value": letter, "status": "invalidated"} for letter in guess]
+
+    for i, letter_obj in enumerate(validated_guess):
+        if sol_array[i] == letter_obj["value"]:
+            sol_array[i] = None
+            validated_guess[i]["status"] = "correct"
+
+
+    for i, letter_obj in enumerate(validated_guess):
+        if letter_obj["status"] == "correct":
+            continue
+
+        try:
+            letter_index = sol_array.index(letter_obj["value"])
+            sol_array[letter_index] = None
+            validated_guess[i]["status"] = "misplaced"
+        except ValueError:
+            validated_guess[i]["status"] = "incorrect"
+    
+
+
+    return jsonify({
+        "validated_guess": validated_guess,
+        "turn": turns,
+        "attempts": ATTEMPTS,
+        "game_over": turns == ATTEMPTS or guess == SELECTED_WORD,
+        "victory": guess == SELECTED_WORD
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
