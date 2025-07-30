bf80f276ffa6f8835032f9d92dac63bfdc32d1a2
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-30 10:33:53 +0800

    style: add styling to keypad after validation

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./components.css";
 import { IKeypad } from "../types";
 
-export const Keypad = ({ handleButtonPress }: IKeypad) => {
+export const Keypad = ({ handleButtonPress, previousLetters }: IKeypad) => {
   const firstRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const secondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const thirdRow = ["Delete", "Z", "X", "C", "V", "B", "N", "M", "Enter"];
@@ -11,7 +11,7 @@ export const Keypad = ({ handleButtonPress }: IKeypad) => {
       <div className="key-row">
         {firstRow.map((item) => (
           <div
-            className="key"
+            className={`key key-${previousLetters[item]}`}
             onClick={() => handleButtonPress(item)}
             key={item}
           >
@@ -22,7 +22,7 @@ export const Keypad = ({ handleButtonPress }: IKeypad) => {
       <div className="key-row">
         {secondRow.map((item) => (
           <div
-            className="key"
+            className={`key key-${previousLetters[item]}`}
             onClick={() => handleButtonPress(item)}
             key={item}
           >
@@ -33,7 +33,7 @@ export const Keypad = ({ handleButtonPress }: IKeypad) => {
       <div className="key-row">
         {thirdRow.map((item) => (
           <div
-            className="key"
+            className={`key key-${previousLetters[item]}`}
             onClick={() => handleButtonPress(item)}
             id={item === "Delete" || item === "Enter" ? "action" : ""}
             key={item}
