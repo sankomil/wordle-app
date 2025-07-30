123eb95d5d909bdb6d1af750a1bd9fc2034ddbf5
Sankalp Khanna <khanna.1058@gmail.com>
2025-07-30 19:14:46 +0800

    fix: only store the solution for the current day, dont need to store user sessions

--- backend/models/session.py
+++ backend/models/session.py
@@ -3,13 +3,8 @@ from app import db
 class Session(db.Model):
     __tablename__ = "Sessions"
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    solution = db.Column(db.String(5), unique=True, nullable=False)
-    turns = db.Column(db.Integer, default=0)
-    game_over = db.Column(db.Boolean, default=False)
-    won = db.Column(db.Boolean, default=False)
-    guesses = db.Column(db.ARRAY(db.String(5)), default=[])
-
+    id = db.Column(db.String(10), primary_key=True)
+    solution = db.Column(db.String(5), nullable=False)
     def __repr__(self):
         return f'<Session {self.id}>'
 
